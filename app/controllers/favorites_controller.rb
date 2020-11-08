0d8f13dff4f8d71f471fb6b3d733fe1721341a3c
七條毅亮 <take.s.3321@gmail.com>
2020-11-08 21:41:35 +0900

    [Update]いいね非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,14 @@
 class FavoritesController < ApplicationController
 	def create
+		@baby = Baby.find(params[:baby_id])
 		@favorites = current_user.favorites.create(baby_id: params[:baby_id])
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		@baby = Baby.find(params[:baby_id])
 		@favorite = current_user.favorites.find_by(baby_id: @baby.id)
 		@favorite.destroy
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
 end
