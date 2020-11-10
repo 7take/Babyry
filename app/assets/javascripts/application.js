0b9c2e7f81e1dd52a04454579167db8e0df8cbf2
七條毅亮 <take.s.3321@gmail.com>
2020-11-10 21:58:51 +0900

    [Update]hamburger_menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,28 +16,65 @@
 //= require jquery
 //= require_tree .
 
+
+
+
+
+
+(function ($) {
+
+
+  /* ------------------------------- */
+
+})(jQuery);
+
+
+/* SP menu */
+function toggleNav() {
+  var body = document.body;
+  var hamburger = document.getElementById('nav_btn');
+  var blackBg = document.getElementById('nav_bg');
+  hamburger.addEventListener('click', function() {
+    body.classList.toggle('nav_open'); //メニュークリックでnav-openというクラスがbodyに付与
+  });
+  blackBg.addEventListener('click', function() {
+    body.classList.remove('nav_open'); //もう一度クリックで解除
+  });
+}
+
+
+
+
+
+
+
 $(document).on('turbolinks:load', function() {
-// オプションを指定してSkipprの実行
-$(".theTarget").skippr({
-  // スライドショーの変化（"fade" or "slide"）
-  transition : 'fade',
-  // 変化にかかる時間（ミリ秒）
-  speed : 1000,
-  // easingの種類
-  easing : 'easeOutQuart',
-  // ナビゲーションの形（"block" or "bubble"）
-  navType : 'block',
-  // 子要素の種類（"div" or "img"）
-  childrenElementType : 'div',
-  // ナビゲーション矢印の表示（trueで表示）
-  arrows : true,
-  // スライドショーの自動再生（falseで自動再生なし）
-  autoPlay : true,
-  // 自動再生時のスライド切替間隔（ミリ秒）
-  autoPlayDuration : 5000,
-  // キーボードの矢印キーによるスライド送りの設定（trueで有効）
-  keyboardOnAlways : true,
-  // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-  hidePrevious : false
+  // オプションを指定してSkipprの実行
+  $(".theTarget").skippr({
+    // スライドショーの変化（"fade" or "slide"）
+    transition : 'fade',
+    // 変化にかかる時間（ミリ秒）
+    speed : 1000,
+    // easingの種類
+    easing : 'easeOutQuart',
+    // ナビゲーションの形（"block" or "bubble"）
+    navType : 'block',
+    // 子要素の種類（"div" or "img"）
+    childrenElementType : 'div',
+    // ナビゲーション矢印の表示（trueで表示）
+    arrows : true,
+    // スライドショーの自動再生（falseで自動再生なし）
+    autoPlay : true,
+    // 自動再生時のスライド切替間隔（ミリ秒）
+    autoPlayDuration : 5000,
+    // キーボードの矢印キーによるスライド送りの設定（trueで有効）
+    keyboardOnAlways : true,
+    // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
+    hidePrevious : false
+  });
+  toggleNav();
+
+  
 });
-});
+
+
